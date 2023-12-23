def7b36f23860fb787852f0aa42fda4d385f33e6
Yao Amegandjin <yaoamegandjin@gmail.com>
2023-12-22 18:37:30 -0600

    first commit

---
+++ helserQODDAutoFiller.bat
@@ -0,0 +1,5 @@
+@echo off
+set "venv_path=C:\Users\yaoam\OneDrive\Documents\Projects\helserQODDAutoFiller\myenv"
+set "script_path=C:\Users\yaoam\OneDrive\Documents\Projects\helserQODDAutoFiller\qoddAutoFiller.py"
+
+"%venv_path%\Scripts\python.exe" "%script_path%"
--- helserQODDAutoFiller/helserQODDAutoFiller.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-cmd /c ".\myenv\Scripts\activate & python qoddAutoFiller.py"
